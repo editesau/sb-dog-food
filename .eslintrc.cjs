d7bfec92b5391a027cd36e5fb3e0be37a500ca21
amorozov <amorozov@am-air.local>
2022-12-08 14:43:32 +0300

    Set eslint rule for jsx components and set base css styles

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,6 +22,7 @@ module.exports = {
     'react/react-in-jsx-scope': 0,
     'react/jsx-props-no-spreading': 0,
     'react/prop-types': 0,
+    'react/function-component-definition': [1, { namedComponents: 'arrow-function' }],
     'no-console': 0,
     'import/no-cycle': 0,
     'import/prefer-default-export': 0,
