71bd64cc56c1a341553769e63fa8c929dfe71a6d
amorozov <amorozov@am-air.local>
2023-01-31 16:27:25 +0300

    Add search input value to searchParams

--- src/components/HeaderSearch/HeaderSearch.jsx
+++ src/components/HeaderSearch/HeaderSearch.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import { useDispatch } from 'react-redux'
 import { useMemo, useState, useEffect } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useSearchParams } from 'react-router-dom'
 import { setFilter, clearFilter } from '../../store/slices/filterProductsSlice/filterProductsSlice'
 import styles from './HeaderSearch.module.scss'
 
@@ -10,6 +10,7 @@ const HeaderSearch = ({ auth }) => {
   const [inputValue, setInputValue] = useState('')
   const dispatch = useDispatch()
   const location = useLocation()
+  const [searchParams, setSearchParams] = useSearchParams()
 
   const debounce = (func, timeout = 500) => {
     let timer
@@ -30,9 +31,29 @@ const HeaderSearch = ({ auth }) => {
     setReduxState('')
   }, [location.pathname])
 
+  useEffect(() => {
+    if (searchParams.has('search')) {
+      const searchValue = searchParams.get('search')
+      setInputValue(searchValue)
+      setReduxState(searchValue)
+    }
+  }, [])
+
   const searchHandler = (event) => {
     setInputValue(event.target.value)
     debounced(event.target.value)
+
+    if (!event.target.value) {
+      searchParams.delete('search')
+      setSearchParams(searchParams)
+    } else {
+      setSearchParams(
+        {
+          ...Object.fromEntries(searchParams.entries()),
+          search: event.target.value,
+        },
+      )
+    }
   }
 
   const clearHandler = () => {
